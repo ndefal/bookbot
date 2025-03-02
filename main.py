c035f32c36b668b4bb0b13813f2855659223ca22
ndefal <ndefalco65@gmail.com>
2025-03-02 00:54:41 -0600

    added addional functions and finished print message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import wordcount
+from stats import charcount
+from stats import sort_letters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -11,7 +13,21 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     
     size = wordcount(book)
+    letter_count = charcount(book)
+    sorted_count = sort_letters(letter_count)
 
-    print(f"{size} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {size} total words")
+    print("--------- Character Count -------")
+    
+    for item in sorted_count:
+        if item['char'].isalpha():
+            print(f"{item['char']}: {item['count']}")
+    
+    print("============= END ===============")
+    
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,29 @@
 def wordcount(book):
     split = book.split()
     return len(split)
-'''
+
 def charcount(book):
-    for letter in book:
-'''
+    letter_count = {}
+    lower_case = book.lower()
+    
+    for letter in lower_case:
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+    
+    return letter_count
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_letters(char_counts):
+    sorted_list = []
+
+    for char, count in char_counts.items():
+        char_dict = {'char' : char, 'count' : count}
+        sorted_list.append(char_dict)
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
