87df955c1f382d8dc34ea37dcfe3001ee4063fed
ndefal <ndefalco65@gmail.com>
2025-03-04 02:15:41 -0600

    updated scripting to run multiple books w/ correct output messages

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import wordcount
 from stats import charcount
 from stats import sort_letters
@@ -10,14 +11,19 @@ def get_book_text(file_path):
 
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book = get_book_text(sys.argv[1])
+      
     size = wordcount(book)
     letter_count = charcount(book)
     sorted_count = sort_letters(letter_count)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {size} total words")
     print("--------- Character Count -------")
